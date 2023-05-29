51204ebb44470668818aae7bb3842eacc74bbfc6
dhruv <dhruvpatel7012@gmail.com>
2023-05-29 11:45:22 +0530

    Teacher Student Structure added

---
+++ src/main/groovy/com/ts/TeacherStudentApplication.groovy
@@ -0,0 +1,13 @@
+package com.ts
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TeacherStudentApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TeacherStudentApplication, args)
+	}
+
+}
